44e507d66d6d7f6b120dbdff6e9fba1665b1217a
myaOlaye <136443656+myaOlaye@users.noreply.github.com>
2025-06-07 12:14:39 +0100

    Set up hosting of be

--- BE/db/connection.js
+++ BE/db/connection.js
@@ -5,12 +5,14 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE set");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 const config = {};
 
-console.log("PGDATABASE:", process.env.PGDATABASE);
-
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 module.exports = new Pool(config);
